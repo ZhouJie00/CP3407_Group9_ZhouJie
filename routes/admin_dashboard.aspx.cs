4aa8ecab007f92d399b6d2ae267828dec88c38cc
Zhou Jie <151333186+ZhouJie00@users.noreply.github.com>
2024-07-31 16:41:44 +0800

    Add initial admin dashboard
    Added admin_dashboard.aspx with the initial HTML structure for managing e-commerce products.

---
+++ routes/admin_dashboard.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace AWAD_Assignment.routes
+{
+    public partial class admin_dashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void GridView_ProductTable_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+        }
+
+        protected void GridView_ProductTable_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+        }
+
+        protected void GridView_ProductTable_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+        }
+
+        protected void GridView_ProductTable_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+        }
+
+        protected void GridView_ProductTable_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+        }
+    }
+}
