6893d4296f0c783ddb3aa5f0e49d084b490529d2
Zhou Jie <151333186+ZhouJie00@users.noreply.github.com>
2024-07-31 17:38:34 +0800

    Add functions for password changing in the correct folder
    functions support the password recovery process by handling token decryption from database, checking token expiration, verifying token validity, and updating user passwords in the database.

--- App_Code/Classes.cs
+++ App_Code/Classes.cs
@@ -94,5 +94,58 @@ public class Clothes {
         }
         return gridView;
     }
+
+    public static string DecryptEmailToken(string textToDecrypt) {
+        try {
+            //string  = "6+PXxVWlBqcUnIdqsMyUHA==";
+            string ToReturn = "";
+            string publickey = "12345678";
+            string secretkey = "87654321";
+            byte[] privatekeyByte = { };
+            privatekeyByte = Encoding.UTF8.GetBytes(secretkey);
+            byte[] publickeybyte = { };
+            publickeybyte = Encoding.UTF8.GetBytes(publickey);
+            MemoryStream ms = null;
+            CryptoStream cs = null;
+            byte[] inputbyteArray = new byte[textToDecrypt.Replace(" ", "+").Length];
+            inputbyteArray = Convert.FromBase64String(textToDecrypt.Replace(" ", "+"));
+            using (DESCryptoServiceProvider des = new DESCryptoServiceProvider()) {
+                ms = new MemoryStream();
+                cs = new CryptoStream(ms, des.CreateDecryptor(publickeybyte, privatekeyByte), CryptoStreamMode.Write);
+                cs.Write(inputbyteArray, 0, inputbyteArray.Length);
+                cs.FlushFinalBlock();
+                Encoding encoding = Encoding.UTF8;
+                ToReturn = encoding.GetString(ms.ToArray());
+            }
+            return ToReturn;
+        } catch (Exception ae) {
+            throw new Exception(ae.Message, ae.InnerException);
+        }
+    }
+
+    public static bool HasEmailTokenExpired(string token, int tokenLifeSpanDays = 3) {
+        var data = Convert.FromBase64String(token);
+        var tokenCreationDate = DateTime.FromBinary(BitConverter.ToInt64(data, 0));
+        return tokenCreationDate < DateTime.UtcNow.AddDays(-tokenLifeSpanDays);
+    }
+
+    public static int GetDecryptedTokenEmailFromDataBase(string decryptedToken)
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Database"].ConnectionString);
+
+        conn.Open();
+
+        string checkuser = "SELECT COUNT(*) FROM Accounts WHERE Email = @email";
+        SqlCommand com = new SqlCommand(checkuser, conn);
+        string email = Encoding.ASCII.GetString(Convert.FromBase64String(decryptedToken)).Substring(8);
+        com.Parameters.AddWithValue("@email", email);
+
+        int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+
+        conn.Close();
+        return temp;
+    }
+
+
  
 }
