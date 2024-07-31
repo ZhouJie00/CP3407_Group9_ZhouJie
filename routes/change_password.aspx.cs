757e3f86f2d0ae1b63a025d8fb81b8267bd48d72
Zhou Jie <151333186+ZhouJie00@users.noreply.github.com>
2024-07-31 17:15:08 +0800

    Add initial code for change password page

---
+++ routes/change_password.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AWAD_Assignment.routes
+{
+    public partial class recover_password : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button_ChangePassword_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
