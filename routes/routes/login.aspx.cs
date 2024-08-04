267388d9cb38574945af1bdee4677d62ccf77ebd
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2024-08-05 04:49:30 +0800

    Add initial code for login page

---
+++ routes/routes/login.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AWAD_Assignment.routes
+{
+    public partial class login : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+ 
+        }
+
+        protected void btnSignIn_Click(object sender, EventArgs e)
+        {
+ 
+        }
+
+        protected void LinkButton_SignInWithGoogle_Click(object sender, EventArgs e)
+        {
+ 
+        }
+    }
+}
