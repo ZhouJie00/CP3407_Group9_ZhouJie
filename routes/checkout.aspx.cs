34e8d3fff907ae705220699d9cb691f4a7584a46
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2024-08-05 05:20:22 +0800

    Add initial code for checkout page

---
+++ routes/checkout.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+
+namespace AWAD_Assignment.routes
+{
+    public partial class checkout : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+                      }
+        }
+
+
+        protected void LinkButton_Payment_Click(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+ 	}
+}
