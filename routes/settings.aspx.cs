08f7461a580c7a6b66ff752fcdc1b53ad7838707
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2024-08-05 05:40:05 +0800

    Add initial code for settings page

---
+++ routes/settings.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Salt_Password_Sample;
+using System.Drawing;
+using QRCoder;
+using System.IO;
+using OtpNet;
+
+namespace AWAD_Assignment.routes {
+    public partial class settings : BasePage {
+        protected void Page_Load(object sender, EventArgs e) {
+
+ 
+        }
+
+        protected void Button_UpdateAccount_Click(object sender, EventArgs e) {
+ 
+        }
+
+        protected void LinkButton_mfa_Click(object sender, EventArgs e) {
+ 
+ 
+        }
+    }
+}
