68afa9a588593cc6b790eef288e87dc252a77b22
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2024-08-05 05:03:46 +0800

    Add initial search page

--- routes/search.aspx.cs
+++ routes/search.aspx.cs
@@ -1,28 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
-using System.Web.UI;
+using System.Diagnostics;
 
 namespace AWAD_Assignment.routes
 {
-    public partial class search : BasePage
-    {
+    public partial class search : BasePage {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                if (!string.IsNullOrWhiteSpace(Request.QueryString["q"]))
-                {
-                    string searchInput = Server.HtmlDecode(Request.QueryString["q"]);
-                    PerformSearch(searchInput);
+            if (Request.QueryString["q"] != "" && Request.QueryString["q"] != null) {
+                string search_input = Server.HtmlDecode(Request.QueryString["q"]);
+                int temp = Function.AnyResultsFromSearch(search_input);
+
+                if (temp > 0) {
+
+
+                    Repeater1 = Function.GetResultFromSearch(Repeater1, search_input);
+
+                    // Remove No results found
+                    Label_NoResultsFound.Visible = false;
+                    Label_NoResultsFound.Text = "";
                 }
-                else
-                {
-                    // Redirect to home if no query provided
-                    Response.Redirect("home");
-                }
-            }
+
+            } else {
+                //Return absolute path with params "q"
+                Response.Redirect("home");
+            }            
         }
     }
 }
