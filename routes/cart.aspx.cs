b7896bd7b5207b5597a7bb4806aebb86cfd3d4f4
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2024-08-04 23:20:05 +0800

    Add initial code for cart page

---
+++ routes/cart.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AWAD_Assignment.routes {
+    public partial class cart : BasePage {
+
+        
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
