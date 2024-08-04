4f44a85b9e9e2f10ea978154f64cc8e8275f92bd
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2024-08-05 01:03:48 +0800

    Add initial code for forget page

---
+++ forget_password.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+using System.Web.UI;
+
+namespace AWAD_Assignment.routes
+{
+    public partial class ForgetPassword : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnForgetPassword_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
