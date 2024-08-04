972426e73ad8fdb70be0686ca4c2fa421e66d5f4
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2024-08-05 05:02:11 +0800

    Add initial code for search page

---
+++ routes/search.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.UI;
+
+namespace AWAD_Assignment.routes
+{
+    public partial class search : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (!string.IsNullOrWhiteSpace(Request.QueryString["q"]))
+                {
+                    string searchInput = Server.HtmlDecode(Request.QueryString["q"]);
+                    PerformSearch(searchInput);
+                }
+                else
+                {
+                    // Redirect to home if no query provided
+                    Response.Redirect("home");
+                }
+            }
+        }
+    }
+}
