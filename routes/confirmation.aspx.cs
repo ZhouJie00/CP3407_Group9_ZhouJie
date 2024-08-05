b519d1af3d8e095b6d95aea67dbfad4d54a87b04
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2024-08-05 22:13:01 +0800

    Add purchase confirmation detail feature
    Display purchase item in details.

---
+++ routes/confirmation.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Diagnostics;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace AWAD_Assignment.routes
+{
+    public partial class confirmation : BasePage {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        { 
+
+        }
+     
+    }
+}
