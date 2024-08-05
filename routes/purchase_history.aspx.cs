8ae623e942c991d30890e4e8f02a4a6da9e2fde5
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2024-08-05 15:35:47 +0800

    Add initial code for purchase history page

---
+++ routes/purchase_history.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AWAD_Assignment.routes
+{
+    public partial class purchase_history : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+ 
+
+        }
+
+        protected void RepeaterHistory_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+ 
+        }
+
+        protected void RepeaterHistoryChild_DataBinding(object sender, EventArgs e)
+        {
+ 
+        }
+    }
+}
