c11ae2f64816ba3c46b94944d3119119ccfea69c
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2024-08-05 23:05:00 +0800

    Add initial code for verify 2fa page
    - Validate the one-time password (OTP) using OtpNet.
    - Redirect users based on their role (admin or regular user) after successful verification.

---
+++ routes/verify2fa.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+ 
+
+namespace AWAD_Assignment.routes {
+    public partial class verify2fa : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+ 
+        }
+
+        protected void LinkButton_2fa_Click(object sender, EventArgs e) {
+
+ 
+        }
+    }
+}
