dcbab39278ea6d3ec568366b53b5ff17ccca60a1
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2024-08-05 15:32:13 +0800

    Add email verification for user registration
    - Implement email verification for user registration.
    - Decrypt and validate email verification tokens.
    - Check token expiration and redirect invalid tokens to a 404 page.
    - Verify the email exists in the database.
    - Update user record to mark the email as verified if the token is valid.

--- routes/verify_email.aspx.cs
+++ routes/verify_email.aspx.cs
@@ -12,7 +12,33 @@ using System.Configuration;
 namespace AWAD_Assignment.routes {
     public partial class verify_email : BasePage {
         protected void Page_Load(object sender, EventArgs e) {
- 
+            // Decrypt Token
+            string decryptedToken = null;
+            try {
+                decryptedToken = Function.DecryptEmailToken(Request.QueryString["token"].ToString());
+            } catch (NullReferenceException) {
+                // redirect to 404 if no token arg
+                Response.Redirect(ResolveClientUrl("../404"));
+            }
+
+            // Check if token has expired, redirect 404 if true
+            if (Function.HasEmailTokenExpired(decryptedToken)) {
+                Response.Redirect(ResolveClientUrl("../404"));
+            }
+
+            // Check if Decrypted Token Email exists in DB
+            string email = Encoding.ASCII.GetString(Convert.FromBase64String(decryptedToken)).Substring(8);
+            int temp = Function.CheckIfUserExists(email); // Convert.ToInt32(com.ExecuteScalar().ToString());
+
+
+            if (temp == 1) { // if email exists inside DB it is a valid
+                // The user has verified their email
+                Function.SetUserVerificationTrue(email);
+            }
+            else {
+                // Invalid token, redirect to 404
+                Response.Redirect(ResolveClientUrl("../404"));
+            }
         }
     }
 }
