7eafc27bfd228d3df6a85af6c442db877a595240
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2024-08-05 15:29:07 +0800

    Add initial code for verify email page

---
+++ routes/verify_email.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace AWAD_Assignment.routes {
+    public partial class verify_email : BasePage {
+        protected void Page_Load(object sender, EventArgs e) {
+ 
+        }
+    }
+}
