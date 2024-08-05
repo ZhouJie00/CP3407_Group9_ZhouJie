a8ef2398d17859a352a609f9475d2101aaccb3c0
Zhou Jie <jie.zhou3@my.jcu.edu.au>
2024-08-06 00:00:23 +0800

    Add initial for female product listing page
    Implement the female product listing page with initial setup.

---
+++ routes/product_list_women.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace AWAD_Assignment.routes
+{
+    public partial class product_list_women : BasePage {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+ 
+        protected void LinkButtonCategory1_Click(object sender, EventArgs e) {
+
+        }
+        protected void LinkButtonCategory2_Click(object sender, EventArgs e) {
+
+        }
+        protected void LinkButtonCategory3_Click(object sender, EventArgs e) {
+
+        }
+        protected void LinkButtonCategory4_Click(object sender, EventArgs e) {
+
+        }
+        protected void LinkButtonCategory5_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
